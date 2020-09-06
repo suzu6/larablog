742ff2ce6cfa5a804111a1591110d50932e117cc
Yuya Suzumura <37053383+suzu6@users.noreply.github.com>
2020-09-06 20:54:42 +0900

    create post and tag tables

---
+++ database/migrations/2020_09_02_122858_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('discription');
+            $table->string('html');
+            $table->string('top_image_url');
+            $table->foreignId('user_id')->comment('auther');
+            $table->softDeletes('deleted_at', 0);
+            $table->boolean('is_draft');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
