faf55424fd4938edbf7a34113d91b454825ed0bb
Yuya Suzumura <37053383+suzu6@users.noreply.github.com>
2020-09-07 22:36:17 +0900

    Add PostSeeder.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # larablog
+
+```sh
+# シーダー作成
+$ php artisan make:seeder UserSeeder
+
+# シーダの実行
+$ composer dump-autoload
+$ php artisan migrate:refresh --seed
+```
---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $post = new Post;
+        $post->title = "タイトル";
+        $post->html = "<p>本文</p>";
+        $post->is_draft = false;
+        $post->save();
+    }
+}
