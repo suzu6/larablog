c72374cbe2d519804c384d73c4cc025a0d0f6a6a
Yuya Suzumura <37053383+suzu6@users.noreply.github.com>
2020-09-13 00:06:05 +0900

    タスクデータ作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TagSeeder::class,
             PostSeeder::class,
+            TasksTableSeeder::class,
         ]);
     }
 }
