e1be8e94e7ace0f19b43a8b9eac1d8987544a8de
Yuya Suzumura <37053383+suzu6@users.noreply.github.com>
2020-09-12 23:49:04 +0900

    タスク編集コンポーネント実装

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -24,7 +24,9 @@
                     </router-link>
                 </td>
                 <td>
-                    <button class="btn btn-success">Edit</button>
+                    <router-link v-bind:to="{name: 'task.edit', params: {taskId: 1}}">
+                        <button class="btn btn-success">Edit</button>
+                    </router-link>
                 </td>
                 <td>
                     <button class="btn btn-danger">Delete</button>
@@ -36,12 +38,14 @@
                 <td>Content2</td>
                 <td>Jiro</td>
                 <td>
-                    <router-link v-bind:to="{name: 'task.show', params: {taskId: 1}}">
+                    <router-link v-bind:to="{name: 'task.show', params: {taskId: 2}}">
                         <button class="btn btn-primary">Show</button>
                     </router-link>
                 </td>
                 <td>
-                    <button class="btn btn-success">Edit</button>
+                    <router-link v-bind:to="{name: 'task.edit', params: {taskId: 2}}">
+                        <button class="btn btn-success">Edit</button>
+                    </router-link>
                 </td>
                 <td>
                     <button class="btn btn-danger">Delete</button>
@@ -53,12 +57,14 @@
                 <td>Content3</td>
                 <td>Saburo</td>
                 <td>
-                    <router-link v-bind:to="{name: 'task.show', params: {taskId: 1}}">
+                    <router-link v-bind:to="{name: 'task.show', params: {taskId: 3}}">
                         <button class="btn btn-primary">Show</button>
                     </router-link>
                 </td>
                 <td>
-                    <button class="btn btn-success">Edit</button>
+                    <router-link v-bind:to="{name: 'task.edit', params: {taskId: 3}}">
+                        <button class="btn btn-success">Edit</button>
+                    </router-link>
                 </td>
                 <td>
                     <button class="btn btn-danger">Delete</button>
